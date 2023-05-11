75c26d0eda7497148c6b578da279d376de79e71e
EvgenBatt <evgenmgid@gmail.com>
2023-05-12 02:10:02 +0300

    Update Tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = 0;
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startChangeBackgroundColor() {
+  timerId = setInterval(changeBackgroundColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopChangeBackgroundColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startChangeBackgroundColor);
+stopBtn.addEventListener('click', stopChangeBackgroundColor);
+
+// Розмітка кнопок
+
+const buttonsDiv = document.createElement('div');
+buttonsDiv.classList.add('buttons');
+buttonsDiv.setAttribute(
+  'style',
+  'display: flex; gap: 50px; justify-content: center; margin-top: 30px;'
+);
+startBtn.style.padding = '20px 50px';
+stopBtn.style.padding = '20px 50px';
+buttonsDiv.append(startBtn, stopBtn);
+
+const body = document.querySelector('body');
+const p = body.querySelector('p');
+p.insertAdjacentElement('afterend', buttonsDiv);
---
+++ src/js/02-timer.js
@@ -0,0 +1,97 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let endDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const chosenDate = selectedDates[0];
+    if (chosenDate <= new Date()) {
+      window.alert('Please choose a date in the future');
+      document.querySelector('[data-start]').setAttribute('disabled', true);
+    } else {
+      endDate = chosenDate;
+      document.querySelector('[data-start]').removeAttribute('disabled');
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function addLeadingZero(value) {
+  return value < 10 ? `0${value}` : `${value}`;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function startTimer(endDate) {
+  const timerDays = document.querySelector('[data-days]');
+  const timerHours = document.querySelector('[data-hours]');
+  const timerMinutes = document.querySelector('[data-minutes]');
+  const timerSeconds = document.querySelector('[data-seconds]');
+
+  function updateTimer() {
+    const timeLeft = endDate - Date.now();
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      timerDays.textContent = '00';
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    timerDays.textContent = addLeadingZero(days);
+    timerHours.textContent = addLeadingZero(hours);
+    timerMinutes.textContent = addLeadingZero(minutes);
+    timerSeconds.textContent = addLeadingZero(seconds);
+  }
+
+  updateTimer();
+  const timerInterval = setInterval(updateTimer, 1000);
+}
+
+document.querySelector('[data-start]').addEventListener('click', () => {
+  startTimer(endDate);
+});
+
+// Розмітка елементів
+
+document.body.style.margin = '50px 30%';
+
+const timerDiv = document.querySelector('.timer');
+timerDiv.setAttribute('style', 'display: flex; gap: 10px; margin-top: 10px;');
+
+const fieldDivs = document.querySelectorAll('.field');
+fieldDivs.forEach(fieldDiv => {
+  fieldDiv.setAttribute(
+    'style',
+    'display: flex; flex-direction: column; align-items: center;'
+  );
+});
+
+const timeValues = document.querySelectorAll('.value');
+timeValues.forEach(timeValue => {
+  timeValue.setAttribute('style', 'font-size: 20px');
+});
